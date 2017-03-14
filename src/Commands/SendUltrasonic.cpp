8a75b7b509310a21f4585d275164ed1eb747b493
FRC4646 Laptop 1 <FRC4646 Laptop 1@localhost>
2017-03-13 19:56:34 -0500

    Add missing file

--- src/Commands/SendUltrasonic.cpp
+++ src/Commands/SendUltrasonic.cpp
@@ -14,6 +14,7 @@ void SendUltrasonic::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void SendUltrasonic::Execute() {
 	backusensor->SendSD();
+	psensor->SendSD();
 }
 
 // Make this return true when this Command no longer needs to run execute()
